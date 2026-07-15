488fc1dde376146f50957ecced77bf9b6188a3b2
Gary Rennie <gazler@gmail.com>
2026-07-15 10:06:55 +0100

    fix(BackBreeze.Ucwidth): fallback to unicode_width if prim_tty missing
    In some environments such as AtomVM, the prim_tty module is not usable.
    Given that Popcorn only works on OTP 26 for now, there are no more
    robust unicode width functions available.

--- lib/back_breeze/ucwidth.ex
+++ lib/back_breeze/ucwidth.ex
@@ -32,15 +32,38 @@ defmodule BackBreeze.Ucwidth do
   def width_codepoint(codepoint) when is_integer(codepoint) and codepoint in 0x25A0..0x25FF, do: 1
 
   def width_codepoint(codepoint) when is_integer(codepoint) do
-    case Process.get({__MODULE__, codepoint}) do
-      nil ->
-        width = :prim_tty.npwcwidth(codepoint)
+    cache_key = {__MODULE__, codepoint}
 
-        Process.put({__MODULE__, codepoint}, width)
+    case Process.get(cache_key) do
+      nil ->
+        width = codepoint_width(codepoint)
+
+        Process.put(cache_key, width)
         width
 
       width ->
         width
+    end
+  end
+
+  defp codepoint_width(codepoint) do
+    # Popcorn bundles :prim_tty, but AtomVM cannot execute its on_load opcode.
+    # Unlike Code.ensure_loaded?/1, this only inspects loaded modules, allowing
+    # AtomVM to fall back while native OTP retains its NIF-backed implementation.
+    if function_exported?(:prim_tty, :npwcwidth, 1) do
+      :prim_tty.npwcwidth(codepoint)
+    else
+      unicode_width(codepoint)
+    end
+  end
+
+  defp unicode_width(codepoint) do
+    case :unicode_util.lookup(codepoint) do
+      %{category: {:other, :control}} -> 0
+      %{category: {:other, :format}} when codepoint != 0x00AD -> 0
+      %{category: {:mark, :non_spacing}} -> 0
+      %{category: {:mark, :enclosing}} -> 0
+      _properties -> if :unicode_util.is_wide(codepoint), do: 2, else: 1
     end
   end
 end
